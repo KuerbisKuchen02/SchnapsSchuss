0be7f8c04bc5cef88352ec254ac151c49d246e64
Justin Wolek <juwol1423@gmail.com>
2025-08-21 22:28:03 +0200

    feat: Make changes in price and amount update the UI

--- SchnapsSchuss/Models/Entities/InvoiceItem.cs
+++ SchnapsSchuss/Models/Entities/InvoiceItem.cs
@@ -1,16 +1,21 @@
 using SQLite;
 using SQLiteNetExtensions.Attributes;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace SchnapsSchuss.Models.Entities;
 
-public class InvoiceItem
+public partial class InvoiceItem : ObservableObject
 {
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
     [ForeignKey(typeof(Invoice))]
     public int InvoiceId { get; set; }
-    public float TotalPrice { get; set; }
-    public int Amount { get; set; }
+
+    [ObservableProperty]
+    private float totalPrice;
+
+    [ObservableProperty]
+    private int amount;
 
     [ManyToOne]
     public Invoice Invoice { get; set; }
