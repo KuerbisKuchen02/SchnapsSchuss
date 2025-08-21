19e218db82f663d6f2f4bfd640074e8360b1b030
Josia Menger <josia.menger@gmx.de>
2025-08-21 16:07:23 +0200

    feat: add generic crud tables

--- SchnapsSchuss/ViewModels/CrudViewModel.cs
+++ SchnapsSchuss/ViewModels/CrudViewModel.cs
@@ -1,16 +1,37 @@
+using SchnapsSchuss.Models.Entities;
+
 namespace SchnapsSchuss.ViewModels;
 
 public class CrudViewModel<T> : BaseViewModel
 {
-    public List<T> Items { get; set; }
+    private List<T> _items;
 
-    private string _title;
-
-    public string Title
+    public List<T> Items
     {
-        get =>  _title;
-        set => SetProperty(ref  this._title, value);
+        get => _items;
+        set => SetProperty(ref _items, value);
     }
     
+    private string _title;
     
+    public string Title
+    {
+        get => _title;
+        set => SetProperty(ref _title, value);
+    }
+    
+    private List<string> _shownColumnNames;
+
+    public List<string> ShownColumnNames
+    {
+        get => _shownColumnNames;
+        set => SetProperty(ref _shownColumnNames, value);
+    }
+
+    public CrudViewModel(string title, List<string> shownColumn)
+    {
+        _title = title;
+        _items = [];
+        _shownColumnNames = shownColumn;
+    }
 }
